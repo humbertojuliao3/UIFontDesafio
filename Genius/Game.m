a02ecea2516238906a670aca54197e7442531a00
SidneyDuarte <sidney_silva13@outlook.com>
2015-02-13 11:08:32 -0200

    Diagrama de classe simples

---
+++ Genius/Game.m
@@ -0,0 +1,13 @@
+//
+//  Game.m
+//  Genius
+//
+//  Created by Sidney Silva on 13/02/15.
+//  Copyright (c) 2015 Sidney Silva. All rights reserved.
+//
+
+#import "Game.h"
+
+@implementation Game
+
+@end
