04db26cb6c786342b332f255d13d50c3b96a09b8
SidneyDuarte <sidney_silva13@outlook.com>
2015-02-15 23:02:52 -0200

    Ateualização com limpeza de tela

---
+++ Genius/Projeto/Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  Projeto
+//
+//  Created by Sidney Silva on 15/02/15.
+//  Copyright (c) 2015 Liliane Bezerra Lima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Game : NSObject
+
+@end
---
+++ Genius/Projeto/Game.m
@@ -0,0 +1,13 @@
+//
+//  Game.m
+//  Projeto
+//
+//  Created by Sidney Silva on 15/02/15.
+//  Copyright (c) 2015 Liliane Bezerra Lima. All rights reserved.
+//
+
+#import "Game.h"
+
+@implementation Game
+
+@end
